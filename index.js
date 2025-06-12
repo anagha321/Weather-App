f482284569582299203d80bd77c1631a9f63863d
anagha321 <anaghas1004@gamil.com>
2025-06-12 19:22:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "92fb249138826014f60a9fc5656235f5";
+
+weatherForm.addEventListener("submit", event => {
+
+    event.preventDefault();
+
+    
+
+});
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+
+}
